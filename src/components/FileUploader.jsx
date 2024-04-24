989c3c97da4f7529647ed1c7d504d2b669c4a14c
BalajiHarinath <89400019+BalajiHarinath@users.noreply.github.com>
2024-04-24 08:10:26 +0530

    refactor: modified FileUploader.jsx

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -1,10 +1,31 @@
+// import React, { useCallback } from 'react';
+// import { useDropzone } from 'react-dropzone';
+
+// const FileUploader = ({ onFilesSelected }) => {
+//   const onDrop = useCallback((acceptedFiles) => {
+//     onFilesSelected(acceptedFiles);
+//   }, []);
+
+//   const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+//   return (
+//     <div {...getRootProps()}>
+//       <input {...getInputProps()} />
+//       <button className="select-files-button">Click to select files</button>
+//     </div>
+//   );
+// };
+
+// export default FileUploader;
+
+
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 
 const FileUploader = ({ onFilesSelected }) => {
   const onDrop = useCallback((acceptedFiles) => {
     onFilesSelected(acceptedFiles);
-  }, []);
+  }, [onFilesSelected]);
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
