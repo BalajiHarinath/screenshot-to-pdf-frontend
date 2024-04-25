6cf8df5f6d456ea108111526fa205fc6c131aff1
BalajiHarinath <89400019+BalajiHarinath@users.noreply.github.com>
2024-04-25 07:38:24 +0530

    refactor: modified FileUploader.jsx

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -1,61 +1,40 @@
-// import React, { useCallback } from 'react';
-// import { useDropzone } from 'react-dropzone';
-
-// const FileUploader = ({ onFilesSelected }) => {
-//   const onDrop = useCallback((acceptedFiles) => {
-//     onFilesSelected(acceptedFiles);
-//   }, []);
-
-//   const { getRootProps, getInputProps } = useDropzone({ onDrop });
-
-//   return (
-//     <div {...getRootProps()}>
-//       <input {...getInputProps()} />
-//       <button className="select-files-button">Click to select files</button>
-//     </div>
-//   );
-// };
-
-// export default FileUploader;
-
-
-// import React, { useCallback } from 'react';
-// import { useDropzone } from 'react-dropzone';
-
-// const FileUploader = ({ onFilesSelected }) => {
-//   const onDrop = useCallback((acceptedFiles) => {
-//     onFilesSelected(acceptedFiles);
-//   }, [onFilesSelected]);
-
-//   const { getRootProps, getInputProps } = useDropzone({ onDrop });
-
-//   return (
-//     <div {...getRootProps()}>
-//       <input {...getInputProps()} />
-//       <button className="select-files-button">Click to select files</button>
-//     </div>
-//   );
-// };
-
-// export default FileUploader;
-
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 const FileUploader = ({ onFilesSelected }) => {
-  const handleFileChange = (e) => {
-    const files = e.target.files;
-    onFilesSelected(files);
-  };
+  const onDrop = useCallback((acceptedFiles) => {
+    onFilesSelected(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <div>
-      <label className="custom-file-upload">
-        <input type="file" onChange={handleFileChange} multiple />
-        Click to select files
-      </label>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <button className="select-files-button">Click to select files</button>
     </div>
   );
 };
 
 export default FileUploader;
+
+// import React from 'react';
+
+// const FileUploader = ({ onFilesSelected }) => {
+//   const handleFileChange = (e) => {
+//     const files = e.target.files;
+//     onFilesSelected(files);
+//   };
+
+//   return (
+//     <div>
+//       <label className="custom-file-upload">
+//         <input type="file" onChange={handleFileChange} multiple />
+//         Click to select files
+//       </label>
+//     </div>
+//   );
+// };
+
+// export default FileUploader;
 
